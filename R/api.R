3bc395f256034282874bbb6ecad336b7d545196d
Danielle Navarro <djnavarro@protonmail.com>
2025-09-28 22:23:42 +1000

    Initial commit

---
+++ R/api.R
@@ -0,0 +1,38 @@
+
+#' Check if an environment is clean
+#'
+#' @param action Behaviour to take if environment is not clean. Possible values are "error", "warn", and "message"
+#' @param ignore Character vector containing names of variables that will not trigger an action. If NULL, variables that start with a dot are ignored
+#' @param envir Environment to be checked
+#'
+#' @returns Invisibly returns a logical vector with names corresponding to object names in the environment. Values are TRUE if the object is ignored, FALSE if not
+#' @export
+#' @examples
+#' check_environment(action = "message")
+#'  
+check_environment <- function(action = "warn", ignore = NULL, envir = .GlobalEnv) {
+  .validate_action(action)
+  .validate_ignore(ignore)
+  status <- .get_environment_status(envir, ignore)
+  msg  <- .message_text("Found variables:", status)
+  .action(action, status, msg)
+}
+
+#' Check if list of loaded packages is clean
+#'
+#' @param action Behaviour to take if environment is not clean. Possible values are "error", "warn", and "message"
+#' @param ignore Character vector containing names of non-base packages that will not trigger an action. If NULL, only the base packages are permitted
+#'
+#' @returns Invisibly returns a logical vector with names corresponding to loaded namespaces. Values are TRUE if the package is ignored, FALSE if not
+#' @export
+#' @examples
+#' check_packages(action = "message")
+#'  
+check_packages <- function(action = "warn", ignore = NULL) {
+  .validate_action(action)
+  .validate_ignore(ignore)
+  status <- .get_package_status(ignore)
+  msg <- .message_text("Found loaded packages:", status)
+  .action(action, status, msg)
+}
+
---
+++ R/internal.R
@@ -0,0 +1,58 @@
+
+.validate_action <- function(action) {
+  stopifnot(
+    "`action` must be one of 'error', 'warn', or 'message'" = length(action) == 1L,
+    "`action` must be one of 'error', 'warn', or 'message'" = is.character(action),
+    "`action` must be one of 'error', 'warn', or 'message'" = action %in% c("error", "warn", "message")
+  )
+}
+
+.validate_ignore <- function(ignore) {
+  stopifnot("`ignore` must be a character vector or NULL" = is.character(ignore) | is.null(ignore))
+}
+
+.get_environment_status <- function(envir, ignore) {
+  obj <- ls(envir = envir, all.names = TRUE)
+  if (is.null(ignore)) ignore <- obj[grepl(pattern = "^\\.", x = obj)]
+  status <- obj %in% ignore
+  names(status) <- obj
+  status
+}
+
+.get_package_status <- function(ignore) {
+  if (is.null(ignore)) ignore <- character(0L)
+  status <- vapply(
+    loadedNamespaces(), 
+    function(x) identical(utils::packageDescription(x)$Priority, "base") | x %in% ignore, 
+    logical(1L)
+  )
+  status
+}
+
+.message_text <- function(prefix, status, max_len = 4L) {
+  lst <- names(status[!status])
+  len <- length(lst)
+  if (len == 0L) return(NULL)
+  if (len <= max_len) {
+    txt <- paste(lst, collapse = ", ")
+  } else {
+    lst <- lst[1:max_len]
+    txt <- paste(lst, collapse = ", ")
+    txt <- paste0(txt, ", and ", len - max_len, " more")
+  }
+  paste(prefix, txt)
+}
+
+.action <- function(action, status, message = NULL) {
+  if (!any(!status)) return(invisible(status))
+  if (action == "message") {
+    message(message)
+    return(invisible(status))
+  } 
+  if (action == "warn") {
+    warning(message, call. = FALSE)
+    return(invisible(status))
+  }
+  stop(message, call. = FALSE)
+}
+
