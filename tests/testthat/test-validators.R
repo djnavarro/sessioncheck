23735b139144abe7eb21e7050942877ca0a117e8
Danielle Navarro <djnavarro@protonmail.com>
2025-10-04 16:37:00 +1000

    adds tests for api use of validators

--- tests/testthat/test-validators.R
+++ tests/testthat/test-validators.R
@@ -23,3 +23,16 @@ test_that("valid `ignore` arguments are permitted", {
   expect_no_error(.validate_ignore(ignore = ""))
   expect_no_error(.validate_ignore(ignore = "sessioncheck"))
 })
+
+test_that("session checkers call the validators", {
+  checkers <- list(
+    check_globalenv, 
+    check_attachments, 
+    check_packages,
+    check_namespaces
+  )
+  for (chk in checkers) {
+    expect_error(chk(action = "none", ignore = 1L))
+    expect_error(chk(action = "asdf", ignore = NULL))
+  }
+})
