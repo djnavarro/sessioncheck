a1465d90ea69650709aa17b824a1123ce877894e
Danielle Navarro <djnavarro@protonmail.com>
2025-10-04 14:16:48 +1000

    consolidate documentation for individual checkers

--- R/api.R
+++ R/api.R
@@ -1,89 +1,93 @@
 
-#' Check if an environment is clean
-#'
-#' @param action Behaviour to take if environment is not clean. Possible values 
-#' are "error", "warn", "message", and "none".
-#' @param ignore Character vector containing names of variables that will not 
-#' trigger an action. If NULL, variables that start with a dot are ignored.
-#' @param envir Environment to be checked.
-#'
-#' @returns Invisibly returns a logical vector with names corresponding to 
-#' object names in the environment. Values are TRUE if the object is ignored, 
-#' FALSE if not
-#' @export
-#' @examples
-#' check_environment(action = "message")
-#'  
-check_environment <- function(action = "warn", ignore = NULL, envir = .GlobalEnv) {
-  .validate_action(action)
-  .validate_ignore(ignore)
-  status <- .get_environment_status(envir, ignore)
-  msg  <- .message_text("Found variables:", status)
-  .action(action, status, msg)
-}
-
-#' Check if package/namespace status is clean
-#'
-#' @param action Behaviour to take if package/namespace status is not clean. 
-#' Possible values are "error", "warn", "message", and "none".
-#' @param ignore Character vector containing names of non-base packages that 
-#' will not trigger an action. If NULL, only the base packages and sessioncheck 
-#' itself are permitted.
-#'
-#' @returns Invisibly returns a logical vector with names corresponding to 
-#' loaded namespaces for `check_namespaces()`, or names of attached packages
-#' for `check_packages()`. Values are TRUE if the package is ignored, FALSE if not.
 #' 
+#' @title Check aspects to the R session
+#' 
+#' @description
+#' Individual session check functions that each inspect one way in which an R
+#' session could be considered not to be "clean". Session checkers can produce
+#' errors, warnings, or messages if requested.
+#' 
+#' @param action Behaviour to take if the status is not clean. Possible values are 
+#' "error", "warn", "message", and "none". The default is `action = "warn"`.
+#' @param ignore Character vector containing names of objects, packages, environment
+#' names that will not trigger an action. Some entities will never trigger actions
+#' (see details).
+#'
+#' @returns Invisibly returns a status vector, a logical vector with names referring
+#' to a detected entity (e.g., object, package, environment). Values are `TRUE` if 
+#' the entity is ignored, `FALSE` if it triggers an action.
+#'  
 #' @examples
 #' check_packages(action = "message")
 #' check_namespaces(action = "message")
+#' check_globalenv(action = "message")
+#' check_attachments(action = "message")
 #'  
-#' @name check_packages
-#' @aliases check_namespaces
+#' @details
+#' The default behaviour of the `ignore` argument is slightly for each checker:
+#' 
+#' - `check_globalenv()`: This checker inspects the state of the global environment
+#' and takes action based on the objects found there. When `ignore = NULL`, variables 
+#' in the global environment will be ignored if the name starts with a dot. For 
+#' example, `.Random.seed` and `.Last.value` no not trigger actions by default.
+#' 
+#' - `check_packages()`: This checker inspects the list of packages that have been
+#' attached to the search path (e.g., via `library()`). When  `ignore = NULL`, R 
+#' packages that have "base" priority (e.g., **base**, **utils**, and **grDevices**)
+#' do not trigger an action. Note that the **base** package itself never triggers an
+#' action. 
+#' 
+#' - `check_namespaces()`: This checker inspects the list of loaded namespaces (e.g.
+#' packages that have been loaded but not attached). The `ignore` argument for this
+#' checker is almost identical to `check_packages()`: the only difference is that 
+#' the **sessioncheck** package itself will never trigger an action.
+#' 
+#' - `check_attachments()`: This checker inspects all environments on the search
+#' path. This includes attached packages, anything added using `attach()`, and the
+#' global environment. When `ignore = NULL`, package environents do not trigger an
+#' action, nor do "tools:rstudio", "tools:positron", or "Autoloads". The global 
+#' environment and the package environment for the **base** package never trigger
+#' actions.
+#' 
+#' @name sessionchecks
 NULL
 
 #' @export
-#' @rdname check_packages
+#' @rdname sessionchecks
+check_globalenv <- function(action = "warn", ignore = NULL) {
+  .validate_action(action)
+  .validate_ignore(ignore)
+  status <- .get_environment_status(envir = .GlobalEnv, ignore)
+  msg  <- .message_text("Found objects:", status)
+  .action(action, status, msg)
+}
+
+#' @export
+#' @rdname sessionchecks
 check_packages <- function(action = "warn", ignore = NULL) {
   .validate_action(action)
   .validate_ignore(ignore)
   status <- .get_package_status(ignore)
-  msg <- .message_text("Found attached packages:", status)
+  msg <- .message_text("Found packages:", status)
   .action(action, status, msg)
 }
 
 #' @export
-#' @rdname check_packages
+#' @rdname sessionchecks
 check_namespaces <- function(action = "warn", ignore = NULL) {
   .validate_action(action)
   .validate_ignore(ignore)
   status <- .get_namespace_status(ignore)
-  msg <- .message_text("Found loaded namespaces:", status)
+  msg <- .message_text("Found namespaces:", status)
   .action(action, status, msg)
 }
 
-#' Check the search path for non-standard attachments
-#'
-#' @param action Behaviour to take if package/namespace status is not clean. 
-#' Possible values are "error", "warn", "message", and "none".
-#' @param ignore Character vector containing names of attached environments that 
-#' will not trigger an action. The global environment never triggers an action.
-#' If `ignore=NULL` the following environment names are ignored: "tools:rstudio",
-#' "tools:positron", "Autoloads"
-#'
-#' @returns Invisibly returns a logical vector with names correspond to the searched
-#' environments as returned by `search()`. Values are always set to `TRUE` for package
-#' environments and the global environment. For all other environments, the values 
-#' are `FALSE` unless the environment belongs to the `ignore` list. 
-#' 
 #' @export
-#' 
-#' @examples
-#' check_attachments(action = "message")
+#' @rdname sessionchecks
 check_attachments <- function(action = "warn", ignore = NULL) {
   .validate_action(action)
   .validate_ignore(ignore)
   status <- .get_attachment_status(ignore)
-  msg <- .message_text("Found attached environments:", status)
+  msg <- .message_text("Found attachments:", status)
   .action(action, status, msg)
 }
