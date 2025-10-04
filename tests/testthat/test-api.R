c31d48af0f0afbb30617dfa544c250ee674ca3a0
Danielle Navarro <djnavarro@protonmail.com>
2025-10-04 16:50:25 +1000

    extends api tests

---
+++ tests/testthat/test-api.R
@@ -0,0 +1,22 @@
+
+test_that("session checkers call the validators", {
+  checkers <- list(
+    check_globalenv, 
+    check_attachments, 
+    check_packages,
+    check_namespaces
+  )
+  for (chk in checkers) {
+    expect_error(chk(action = "none", ignore = 1L))
+    expect_error(chk(action = "asdf", ignore = NULL))
+  }
+})
+
+test_that("session checkers match the correct internal function", {
+
+  expect_equal(check_attachments("none"), .get_attachment_status(NULL))
+  expect_equal(check_globalenv("none"), .get_globalenv_status(NULL))
+  expect_equal(check_namespaces("none"), .get_namespace_status(NULL))
+  expect_equal(check_packages("none"), .get_package_status(NULL))
+
+})
