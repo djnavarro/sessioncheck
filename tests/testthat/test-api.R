44aa6d1b5f4bf5307a830606afdff995ad4bc819
Danielle Navarro <djnavarro@protonmail.com>
2025-11-16 10:39:37 +1100

    adds minimal test for sessioncheck options

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -30,3 +30,16 @@ test_that("sessioncheck() returns a list of status checks", {
 
   expect_equal(ss, ii)
 })
+
+test_that("sessioncheck() respects user options", {
+  opts <- options(sessioncheck = list(action = "none", checks = c("globalenv_objects", "loaded_namespaces")))
+  ss <- sessioncheck()
+  ii <- list(
+    globalenv = check_globalenv_objects(action = "none", allow_globalenv_objects = NULL),
+    namespaces = check_loaded_namespaces(action = "none", allow_loaded_namespaces = NULL)
+  )
+  class(ii) <- "sessioncheck_sessioncheck"
+
+  expect_equal(ss, ii)
+  options(opts)
+})
